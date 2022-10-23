8e113de86b2883c3767fed08e4710aa5318d9c72
Ivan Lamb <31833465+ToastedTaco@users.noreply.github.com>
2022-10-23 16:54:20 -0400

    Update UserInterface.py

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
 import matplotlib
 import pandas as pd
 
@@ -12,18 +12,24 @@ class UserInterface(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Sensor Data')
-        self.charts = ['bar', 'line']
-
-        frame= ttk.Frame(self)
-        frame.pack(side=tk.TOP, expand=1, anchor='w')
+        self.geometry('1000x800')
 
 
         for x in range(3):
+            frame = Frame(self)
+
+            frame.pack(side=tk.TOP, expand=1, anchor='w')
+
             figure = Figure(figsize=(6, 4), dpi=100)
 
             figure_canvas = FigureCanvasTkAgg(figure, self)
 
             figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+            button1 = Button(frame, text='Summarize')
+            button1.pack(side = RIGHT)
+            button2 = Button(frame, text='Aggregate')
+            button2.pack(side = RIGHT)
             self.axes = figure.add_subplot()
 
 
@@ -32,12 +38,9 @@ class UserInterface(tk.Tk):
             # function to add data to graphs
             # self.df= pd.DataFrame(data, columns=['language', 'popularity'])
 
-
-
     def callback(self):
         self.axes.clear()
-        kind = self.charts[self.cbx.current()]
-        self.df.plot(y='y-axis', x='time', kind=kind, ax=self.axes)
+        self.df.plot(y='y-axis', x='time', ax=self.axes)
         self.axes.set_title('Sensor Data')
         self.axes.set_ylabel('Y-Axis')
         self.axes.figure.canvas.draw()
