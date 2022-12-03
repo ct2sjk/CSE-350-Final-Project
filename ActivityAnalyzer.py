4bb56321089c37309593559e41df3e8aa9c02a14
Ivan Lamb <31833465+ToastedTaco@users.noreply.github.com>
2022-12-03 18:43:01 -0500

    update

--- ActivityAnalyzer.py
+++ ActivityAnalyzer.py
@@ -8,7 +8,7 @@ import UserInterface as UI
 if __name__ == '__main__':
     sd = SD.sensorData('DummyData.csv', 'MetaData.csv')
     sd.compileSensor()
-    sd.compileMeta()
+    #sd.compileMeta()
     gd = sd.compileGraphData()
 
     ui = UI.UserInterface(sd,gd)
