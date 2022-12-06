3ed82f1e59d6eeadc0901e93d814274c8fd6efa8
Meow <uzairseyal17@gmail.com>
2022-12-06 18:46:50 -0500

    query frame

--- UserInterface.py
+++ UserInterface.py
@@ -160,7 +160,7 @@ class UserInterface(tk.Tk):
 
         label1 = Label(QueryFrame, text="Query:")
         label1.pack()
-
+        QueryFrame.grid(row=1, column=2, padx=10, pady=5)
 
         inputtxt = tk.Text(QueryFrame,
                            height=2,
@@ -181,11 +181,11 @@ class UserInterface(tk.Tk):
 
         aggregate = Button(QueryFrame, text='Aggregate')
         aggregate.pack(side=TOP)
-        QueryFrame.grid(row=1, column=2, padx=10, pady=5)
+
 
         Compile_Frame = Frame(window_frame, width=100, height=200, padx=10, pady=5)
-        Compile_Frame.grid(row=1, column=4, padx=10, pady=5)
-        label1 = Label(QueryFrame, text="Menu:", padx=20, pady=100)
+        Compile_Frame.grid(row=2, column=2, padx=10, pady=5)
+        label1 = Label(Compile_Frame, text="Menu:", padx=10, pady=5)
         label1.pack()
 
         plt.show()
