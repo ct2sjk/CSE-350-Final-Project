3a516fadac3a75b04fa650470c8f332073da5bd4
Meow <uzairseyal17@gmail.com>
2022-12-06 18:38:15 -0500

    query update

--- UserInterface.py
+++ UserInterface.py
@@ -185,7 +185,7 @@ class UserInterface(tk.Tk):
 
         Compile_Frame = Frame(window_frame, width=100, height=200, padx=10, pady=5)
         Compile_Frame.grid(row=1, column=4, padx=10, pady=5)
-        label1 = Label(QueryFrame, text="Compile Window", padx=10, pady=5)
+        label1 = Label(QueryFrame, text="Menu:", padx=10, pady=5)
         label1.pack()
 
         plt.show()
