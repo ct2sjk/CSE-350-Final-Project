9d40106eb4e5b2299a27a2a435f6d660bdb53323
Ivan Lamb <31833465+ToastedTaco@users.noreply.github.com>
2022-12-08 17:09:31 -0500

    fixed regraph and callback

--- UserInterface.py
+++ UserInterface.py
@@ -35,35 +35,24 @@ class UserInterface(tk.Tk):
         window_frame.pack(expand=True, fill=BOTH)
 
         tempFrame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(tempFrame, text="Temperature")
         label1.pack()
-
         tempFrame.grid(row=0, column=0, padx=10, pady=5)
-
-        figure1 = plt.figure(figsize=(3, 2), dpi=100)
-
-        tempFigure_canvas = FigureCanvasTkAgg(figure1, tempFrame)
-
+        self.figure1 = plt.figure(figsize=(3, 2), dpi=100)
+        tempFigure_canvas = FigureCanvasTkAgg(self.figure1, tempFrame)
         toolbar = NavigationToolbar2Tk(tempFigure_canvas, tempFrame)
         toolbar.update()
         tempFigure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         temp_summarize = Button(
             tempFrame, text='Summarize', command=self.temp_summarize)
         temp_summarize.pack(side=RIGHT)
-        self.axTemp = figure1.add_subplot()
-        self.gd.dfTemp.plot(x='DateTime', y='Temp', ax=self.axTemp)
 
         acc_magFrame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(acc_magFrame, text="Acc Magnitude Average")
         label1.pack()
-
         acc_magFrame.grid(row=1, column=0, padx=10, pady=5)
-
-        figure2 = plt.figure(figsize=(3, 2), dpi=100)
-
-        acc_magFrame_canvas = FigureCanvasTkAgg(figure2, acc_magFrame)
+        self.figure2 = plt.figure(figsize=(3, 2), dpi=100)
+        acc_magFrame_canvas = FigureCanvasTkAgg(self.figure2, acc_magFrame)
         toolbar = NavigationToolbar2Tk(acc_magFrame_canvas, acc_magFrame)
         toolbar.update()
         acc_magFrame_canvas.get_tk_widget().pack(
@@ -71,19 +60,13 @@ class UserInterface(tk.Tk):
         acc_summarize = Button(
             acc_magFrame, text='Summarize', command=self.acc_mag_summarize)
         acc_summarize.pack(side=RIGHT)
-        self.axACC = figure2.add_subplot()
-        self.gd.dfAcc.plot(x='DateTime', y='ACC Magnitude', ax=self.axACC)
 
         on_wrist_Frame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(on_wrist_Frame, text="On Wrist")
         label1.pack()
-
         on_wrist_Frame.grid(row=2, column=0, padx=10, pady=5)
-
-        figure3 = plt.figure(figsize=(3, 2), dpi=100)
-
-        on_wrist_Frame_canvas = FigureCanvasTkAgg(figure3, on_wrist_Frame)
+        self.figure3 = plt.figure(figsize=(3, 2), dpi=100)
+        on_wrist_Frame_canvas = FigureCanvasTkAgg(self.figure3, on_wrist_Frame)
         toolbar = NavigationToolbar2Tk(on_wrist_Frame_canvas, on_wrist_Frame)
         toolbar.update()
         on_wrist_Frame_canvas.get_tk_widget().pack(
@@ -91,19 +74,13 @@ class UserInterface(tk.Tk):
         on_wrist_summarize = Button(
             on_wrist_Frame, text='Summarize', command=self.on_wrist_summarize)
         on_wrist_summarize.pack(side=RIGHT)
-        self.axOnWrist = figure3.add_subplot()
-        self.gd.dfOnWrist.plot(x='DateTime', y='On Wrist', ax=self.axOnWrist)
 
         step_count_Frame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(step_count_Frame, text="Step Count")
         label1.pack()
-
         step_count_Frame.grid(row=0, column=2, padx=10, pady=5)
-
-        figure4 = plt.figure(figsize=(3, 2), dpi=100)
-
-        step_count_Frame_canvas = FigureCanvasTkAgg(figure4, step_count_Frame)
+        self.figure4 = plt.figure(figsize=(3, 2), dpi=100)
+        step_count_Frame_canvas = FigureCanvasTkAgg(self.figure4, step_count_Frame)
         toolbar = NavigationToolbar2Tk(
             step_count_Frame_canvas, step_count_Frame)
         toolbar.update()
@@ -112,55 +89,39 @@ class UserInterface(tk.Tk):
         step_count_summarize = Button(
             step_count_Frame, text='Summarize', command=self.step_count_summarize)
         step_count_summarize.pack(side=RIGHT)
-        self.axStepCt = figure4.add_subplot()
-        self.gd.dfStepCt.plot(x='DateTime', y='Step Count', ax=self.axStepCt)
 
         rest_Frame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(rest_Frame, text="Rest")
         label1.pack()
-
         rest_Frame.grid(row=0, column=1, padx=10, pady=5)
-
-        figure5 = plt.figure(figsize=(3, 2), dpi=100)
-
-        rest_Frame_canvas = FigureCanvasTkAgg(figure5, rest_Frame)
+        self.figure5 = plt.figure(figsize=(3, 2), dpi=100)
+        rest_Frame_canvas = FigureCanvasTkAgg(self.figure5, rest_Frame)
         toolbar = NavigationToolbar2Tk(rest_Frame_canvas, rest_Frame)
         toolbar.update()
         rest_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         rest_summarize = Button(
             rest_Frame, text='Summarize', command=self.rest_summarize)
         rest_summarize.pack(side=RIGHT)
-        self.axRest = figure5.add_subplot()
-        self.gd.dfRest.plot(x='DateTime', y='Rest', ax=self.axRest)
 
         EDA_Frame = Frame(window_frame, width=100, height=200)
-
         label1 = Label(EDA_Frame, text="EDA Average")
         label1.pack()
-
         EDA_Frame.grid(row=1, column=1, padx=10, pady=5)
-
-        figure6 = plt.figure(figsize=(3, 2), dpi=100)
-
-        EDA_Frame_canvas = FigureCanvasTkAgg(figure6, EDA_Frame)
+        self.figure6 = plt.figure(figsize=(3, 2), dpi=100)
+        EDA_Frame_canvas = FigureCanvasTkAgg(self.figure6, EDA_Frame)
         toolbar = NavigationToolbar2Tk(EDA_Frame_canvas, EDA_Frame)
         toolbar.update()
         EDA_Frame_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         EDA_summarize = Button(EDA_Frame, text='Summarize',
                                command=self.EDA_summarize)
         EDA_summarize.pack(side=RIGHT)
-        self.axEDA = figure6.add_subplot()
-        self.gd.dfEDA.plot(x='DateTime', y='EDA', ax=self.axEDA)
 
         Movement_Frame = Frame(window_frame, width=100, height=200)
         label1 = Label(Movement_Frame, text="Movement Intensity")
         label1.pack()
         Movement_Frame.grid(row=2, column=1, padx=10, pady=5)
-
-        figure7 = plt.figure(figsize=(3, 2), dpi=100)
-
-        Movement_Frame_canvas = FigureCanvasTkAgg(figure7, Movement_Frame)
+        self.figure7 = plt.figure(figsize=(3, 2), dpi=100)
+        Movement_Frame_canvas = FigureCanvasTkAgg(self.figure7, Movement_Frame)
         toolbar = NavigationToolbar2Tk(Movement_Frame_canvas, Movement_Frame)
         toolbar.update()
         Movement_Frame_canvas.get_tk_widget().pack(
@@ -168,9 +129,7 @@ class UserInterface(tk.Tk):
         Movement_summarize = Button(
             Movement_Frame, text='Summarize', command=self.Movement_summarize)
         Movement_summarize.pack(side=RIGHT)
-        self.axMovInten = figure7.add_subplot()
-        self.gd.dfMovInten.plot(
-            x='DateTime', y='Movement Intensity', ax=self.axMovInten)
+     
 
         QueryFrame = Frame(window_frame, width=100, height=200)
 
@@ -240,8 +199,7 @@ class UserInterface(tk.Tk):
         Delete = Button(Output_Frame, text="Delete", command=lambda: self.outputtxt1.delete(1.0, END))
         Delete.pack()
 
-        plt.show()
-
+        self.regraph()
     def addNewData(self):
         # add window for compiling new data with two fields
         # fields open a browse window for file explorer to specify files
@@ -306,6 +264,7 @@ class UserInterface(tk.Tk):
 
     def temp_summarize(self):
         # returnData = [sum, highest, lowest]
+        self.callback()
         returnData = self.sd.summarize('Temp')
         # format return data
         Temp_String = self.stringFormat("Temperature", returnData)
@@ -313,6 +272,7 @@ class UserInterface(tk.Tk):
 
     def acc_mag_summarize(self):
         # returnData = [sum, highest, lowest]
+        self.regraph()
         returnData = self.sd.summarize('ACCMagnitude')
         # format return data
         ACC_Mag_String = self.stringFormat("ACC_Magnitude", returnData)
@@ -360,21 +320,29 @@ class UserInterface(tk.Tk):
         return strFormat
 
     def callback(self):
-        self.axTemp.cla()
-        self.axACC.cla()
-        self.axOnWrist.cla()
-        self.axStepCt.cla()
-        self.axRest.cla()
-        self.axEDA.cla()
-        self.axMovInten.cla()
+        self.figure1.clf()
+        self.figure2.clf()
+        self.figure3.clf()
+        self.figure4.clf()
+        self.figure5.clf()
+        self.figure6.clf()
+        self.figure7.clf()
+        plt.show()
 
     def regraph(self):
+        self.axTemp = self.figure1.add_subplot()
+        self.axACC = self.figure2.add_subplot()
+        self.axOnWrist = self.figure3.add_subplot()
+        self.axStepCt = self.figure4.add_subplot()
+        self.axRest = self.figure5.add_subplot()
+        self.axEDA = self.figure6.add_subplot()
+        self.axMovInten = self.figure7.add_subplot()
+
         self.gd.dfTemp.plot(x='DateTime', y='Temp', ax=self.axTemp)
         self.gd.dfAcc.plot(x='DateTime', y='ACC Magnitude', ax=self.axACC)
         self.gd.dfEDA.plot(x='DateTime', y='EDA', ax=self.axEDA)
         self.gd.dfOnWrist.plot(x='DateTime', y='On Wrist', ax=self.axOnWrist)
-        self.gd.dfMovInten.plot(
-            x='DateTime', y='Movement Intensity', ax=self.axMovInten)
+        self.gd.dfMovInten.plot(x='DateTime', y='Movement Intensity', ax=self.axMovInten)
         self.gd.dfStepCt.plot(x='DateTime', y='Step Count', ax=self.axStepCt)
         self.gd.dfRest.plot(x='DateTime', y='Rest', ax=self.axRest)
         plt.show()    
